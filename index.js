a33ba0061bf55a11cdef30fca68fe596e6087506
nico <nnnnicholas@users.noreply.github.com>
2020-06-06 17:16:51 -0400

    It works! The commented out POCKET section at the bottom of index.js is just to acquire the access token. It needs only be executed once, unless revoked.

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 require('dotenv').config();
 const pinboardKey = process.env.PINBOARDAPIKEY;
 const pocketKey = process.env.POCKETCONSUMERKEY;
+const pocketToken = process.env.POCKETACCESSTOKEN;
 const fs = require('fs');
 const axios = require('axios').default;
-let request_token; // for Pocket
+let request_token; // Pocket consumer request token
 let lastUpdate; // last time Pinboard was queried
 let now; // current time key-value pair
 
 //Update every 5 minutes
-// setInterval(update, 300000);
-
-update();
+setInterval(update, 30000);
 
 function update() {
     // FETCH LASTUPDATE TIME
@@ -19,14 +18,13 @@ function update() {
         data = JSON.parse(data);
         if (data.lastUpdate) {
             lastUpdate = new Date(data.lastUpdate);
-            console.log(lastUpdate);
+            console.log(lastUpdate.toString());
             console.log("^^^^ lastUpdate ^^^^^")
         }
     })
     //PINBOARD//
     // Fetch 20 most recent pins in json format.
-    axios.defaults.baseURL = 'https://api.pinboard.in/v1';
-    axios.get('/posts/recent', {
+    axios.get('https://api.pinboard.in/v1/posts/recent', {
             params: {
                 auth_token: pinboardKey,
                 count: 20,
@@ -40,40 +38,61 @@ function update() {
                 // If the post has been added since last update
                 if (new Date(post.time) > lastUpdate) {
                     // And is marked To Read
-                    // if (post.toread === true) {
+                    if (post.toread == "yes") {
                         console.log("this post will be sent to pocket");
                         console.log(post);
                         //send to pocket
-                    // }
+                        axios.post('https://getpocket.com/v3/add', {
+                                url: post.href,
+                                title: post.description,
+                                time: Math.floor(new Date(post.time).getTime() / 1000),
+                                consumer_key: pocketKey,
+                                access_token: pocketToken
+                            })
+                            .then((response) => {
+                                console.log("Pocket Add response:");
+                                console.log(response.status);
+                            })
+                            .catch(function(error) {
+                                console.log(error);
+                            });
+                    }
                 }
             })
+            // SAVE LASTUPDATE TIME
+            now = { lastUpdate: new Date() };
+            fs.writeFile('state.json', JSON.stringify(now), (err, data) => {
+                if (err) { return console.error(err); }
+            });
         })
         .catch(function(error) {
             console.log(error);
         });
 
-    //SAVE LASTUPDATE TIME
-    // now = { lastUpdate: new Date() };
-    // fs.writeFile('state.json', JSON.stringify(now), (err, data) => {
-    //     if (err) { return console.error(err); }
-    // })
 }
 
-
-
-
-//POCKET//
-
+//POCKET AUTH//
 // axios.post('https://getpocket.com/v3/oauth/request', {
-// 	consumer_key:pocketKey,
-// 	// redirect_uri: /// TKTKTK 
-//   })
-//   .then(function (response) {
-
-//   	console.log("response code is");
-//   	console.log(response.code);
-//     request_token = response.code;
-//   })
-//   .catch(function (error) {
-//     console.log(error);
-//   });
+//         consumer_key: pocketKey,
+//         redirect_uri: "https%3A%2F%2Fgoogle.com"
+//     })
+//     .then((response) => {
+//         console.log("Pocket server response: " + response.status);
+//         console.log("Pocket response code: " + response.data);
+//         request_token = response.data.substring(5);
+//         console.log("Visit this address and confirm Pocket authorization request.");
+//         console.log("https://getpocket.com/auth/authorize?request_token=" + request_token + "&redirect_uri=https%3A%2F%2Fgoogle.com");
+//         setTimeout(() => {
+//             axios.post("https://getpocket.com/v3/oauth/authorize", {
+//                     consumer_key: pocketKey,
+//                     code: request_token
+//                 })
+//                 .then((response) => {
+//                     console.log("Pocket Access Token response:")
+//                     console.log(response.data)
+//                 })
+//         }, 20000);
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//     });
